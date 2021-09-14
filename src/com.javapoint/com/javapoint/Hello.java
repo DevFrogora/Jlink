301b99ef00ebaf44ca5cc052ab557027371392a2
neverDieAgain <rr4428310@gmail.com>
2021-09-14 08:46:03 +0530

    jlink Test

---
+++ src/com.javapoint/com/javapoint/Hello.java
@@ -0,0 +1,8 @@
+package com.javapoint;
+public class Hello
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Jlink Demo");
+	}
+}
---
+++ src/com.javapoint/module-info.java
@@ -0,0 +1,3 @@
+module com.javapoint{
+	//keep empty
+}
